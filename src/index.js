e20ea1da5c5f007a43681379d88a7d2c230da3c1
jaeeun <jaeeun9862@gmail.com>
2024-08-05 17:17:21 +0900

    counter 작동하기 + subscribe()

--- src/index.js
+++ src/index.js
@@ -1,23 +1,65 @@
 import { createStore } from 'redux';
 
-// reducer는 무조건 함수형태, data를 가공하는 곳
-// action : 메세지
-const reducer = (state = 0, action) => {
-  console.log(state, action);
-  if (action.type === 'ADD') {
-    return state + 1;
-  } else if (action.type === 'MINUS') {
-    return state - 1;
+// =====================================================
+// =====================================================
+// 1. redux 사용해보기
+// =====================================================
+// // reducer는 무조건 함수형태, data를 가공하는 곳
+// // action : 메세지
+// const reducer = (state = 0, action) => {
+//   console.log(state, action);
+//   if (action.type === 'ADD') {
+//     return state + 1;
+//   } else if (action.type === 'MINUS') {
+//     return state - 1;
+//   } else {
+//     return state;
+//   }
+// };
+
+// // data를 저장하는 곳
+// const store = createStore(reducer);
+
+// console.log(store.getState()); // 0
+
+// // dispatch를 통해 reducer로 action(메세지, 객체형태)을 보냄
+// store.dispatch({ type: 'ADD' });
+// store.dispatch({ type: 'MINUS' });
+
+// =====================================================
+// =====================================================
+// 2. count 변경해보기
+// =====================================================
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.getElementById('number');
+
+const countModify = (count = 0, action) => {
+  if (action.type === 'add') {
+    return count + 1;
+  } else if (action.type === 'minus') {
+    return count - 1;
   } else {
-    return state;
+    return count;
   }
 };
 
-// data를 저장하는 곳
-const store = createStore(reducer);
+const countStore = createStore(countModify);
 
-console.log(store.getState()); // 0
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-// dispatch를 통해 reducer로 action(메세지, 객체형태)을 보냄
-store.dispatch({ type: 'ADD' });
-store.dispatch({ type: 'MINUS' });
+// subscribe : countStore에 변화가 있을 때마다 실행되는 함수
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: 'add' });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: 'minus' });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
