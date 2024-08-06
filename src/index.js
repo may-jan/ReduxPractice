b6ae7d8542cfabca0d9f85ce2d12a826dc41d6b9
jaeeun <jaeeun9862@gmail.com>
2024-08-07 00:05:36 +0900

    Delete To Do

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import { createStore } from 'redux';
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+const addToDo = (text) => {
+  return { type: ADD_TODO, text: text, id: Date.now() };
+};
+
+const deleteToDo = (id) => {
+  return { type: DELETE_TODO, id: id };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: action.id }]; // 새로운 state를 만들어서 return
+      return [{ text: action.text, id: action.id }, ...state]; // 새로운 state를 만들어서 return
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id); // filter : 새로운 array 생성
     default:
       return state;
   }
@@ -21,11 +30,37 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach((toDo) => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener('submit', onSubmit);
