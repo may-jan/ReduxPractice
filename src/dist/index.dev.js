9a76d3756fa0caa4b0be52d93a816a5951755949
jaeeun <jaeeun9862@gmail.com>
2024-08-09 17:50:55 +0900

    React-Redux SetUp

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+var _App = _interopRequireDefault(require("./components/App"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
